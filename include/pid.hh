cd8f4ebd0ef267e70237a6cf5ffa290a7ea6802c
amateurstock <immanuellefyan@gmail.com>
2025-09-05 12:32:05 +0800

    Just some small additions. Too lazy rn

--- include/pid.hh
+++ include/pid.hh
@@ -2,6 +2,24 @@
 #define PID_HH
 #include <Arduino.h>
 
-
+class PID {
+private:
+    float _k_p = 0.0f;
+    float _k_i = 0.0f;
+    float _k_d = 0.0f;
+    float _setpoint = 0.0f;
+    uint32_t _last_time = 0;
+    uint32_t _current_time = 0;
+    uint32_t get_dt();
+public:
+    PID(
+        float k_p,
+        float k_i,
+        float k_d,
+        float setpoint
+    );
+    void initialize(uint32_t start_time);
+    float calculate(uint32_t current_time);
+};
 
 #endif
--- src/pid.cc
+++ src/pid.cc
@@ -1,3 +1,23 @@
 #include "pid.hh"
 
+PID::PID(
+    float k_p,
+    float k_i,
+    float k_d,
+    float setpoint
+) {
+    _k_p = k_p;
+    _k_i = k_i;
+    _k_d = k_d;
+    _setpoint = setpoint;
+}
 
+void PID::initialize(uint32_t start_time) {
+    _last_time = start_time;
+}
+
+uint32_t get_dt() {
+}
+
+float PID::calculate(uint32_t current_time) {
+}
