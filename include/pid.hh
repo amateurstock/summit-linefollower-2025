030c800ee979a26586ff53c2438afd92aecf192c
amateurstock <immanuellefyan@gmail.com>
2025-09-08 21:54:47 +0800

    PID implementation finished.
    This PID implementation was tested on an "incubator model" environment
    (albeit very simple). This is just a barebones implementation. Anyone
    may edit this to fit our specific needs.
    
    The float feedback signal and the float output signal is primarily
    arbitrary, change this as we see fit. We probably woudldn't need a
    setpoint changer anyway since we always want the bot facing where the
    line should be.

--- include/pid.hh
+++ include/pid.hh
@@ -1,25 +1,35 @@
 #ifndef PID_HH
 #define PID_HH
+
 #include <Arduino.h>
 
 class PID {
 private:
+    float _last_error = 0.0f;
+    float _error_total = 0.0f;
+    float _time_scale = 1.0f;
+    uint32_t _last_time = 0;
+    uint32_t _current_time = 0;
+
+    uint32_t get_dt(uint32_t current_time);
+
+public:
     float _k_p = 0.0f;
     float _k_i = 0.0f;
     float _k_d = 0.0f;
     float _setpoint = 0.0f;
-    uint32_t _last_time = 0;
-    uint32_t _current_time = 0;
-    uint32_t get_dt();
-public:
-    PID(
-        float k_p,
+
+    PID(float k_p,
         float k_i,
         float k_d,
-        float setpoint
-    );
-    void initialize(uint32_t start_time);
-    float calculate(uint32_t current_time);
+        float setpoint,
+        float time_scale);
+    float calculate(uint32_t current_time, float signal);
+    void initialize(uint32_t current_time);
+    void update_setpoint(float new_setpoint);
+    void update_constants(float k_p,
+                          float k_i,
+                          float k_d);
 };
 
 #endif
--- src/pid.cc
+++ src/pid.cc
@@ -1,23 +1,78 @@
 #include "pid.hh"
 
-PID::PID(
-    float k_p,
-    float k_i,
-    float k_d,
-    float setpoint
-) {
+/*
+ * @desc: PID constructor. Sets the object's params to the specified args.
+ * @param k_p: Constant for proprotional.
+ * @param k_i: Constant for integral.
+ * @param k_d: Constant for differential.
+ * @param setpoint: Target value for PID to achieve
+ * @param time_scale: Tweaks effect of time-sensitive calculations.
+ */
+PID::PID(float k_p,
+         float k_i,
+         float k_d,
+         float setpoint,
+         float time_scale) {
     _k_p = k_p;
     _k_i = k_i;
     _k_d = k_d;
     _setpoint = setpoint;
+    _time_scale = time_scale;
 }
 
-void PID::initialize(uint32_t start_time) {
-    _last_time = start_time;
+uint32_t PID::get_dt(uint32_t current_time) {
+    _last_time = _current_time;
+    _current_time = current_time;
+    return (_current_time - _last_time);
 }
 
-uint32_t get_dt() {
+void PID::initialize(uint32_t current_time) {
+    _current_time = current_time;
 }
 
-float PID::calculate(uint32_t current_time) {
+/*
+ * @desc: Instructs the object to calculate the resulting PID feedback value.
+ * @param current_time: Timestamp @ calculation time. Recommended: use millis().
+ * @param signal: Feedback signal coming from sensors. Must be in float value.
+ *                Otherwise, cast using (float).
+ */
+float PID::calculate(uint32_t current_time, float signal) {
+    float error = _setpoint - signal;
+    float ret = 0.0f;
+    float dt = (float) get_dt(current_time);
+
+    // Proportional Section
+    ret += _k_p * error;
+
+    // Integral Section
+    _error_total += error * dt * _time_scale;
+    ret += _k_i * _error_total;
+
+    // Differential Section
+    ret += _k_d * (error - _last_error) / (dt * _time_scale);
+    _last_error = error;
+
+    return ret;
 }
+
+/*
+ * @desc: Change PID object's setpoint value.
+ * @param new_setpoint: A new setpoint value.
+ */
+void PID::update_setpoint(float new_setpoint) {
+    _setpoint = new_setpoint;
+}
+
+/*
+ * @desc: Update's PID object's PID constant values.
+ * @param k_p: A new proportional constant value.
+ * @param k_i: A new integral constant value.
+ * @param k_d: A new differential constant value.
+ */
+void PID::update_constants(float k_p,
+                           float k_i,
+                           float k_d) {
+    _k_p = k_p;
+    _k_i = k_i;
+    _k_d = k_d;
+};
